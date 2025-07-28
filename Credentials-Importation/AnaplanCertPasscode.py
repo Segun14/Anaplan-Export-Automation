d7b698a088b1a5a1a681f56be6adb993479fe6e8
Segun14 <108934783+Segun14@users.noreply.github.com>
2025-07-28 18:45:17 +0100

    Add files via upload

---
+++ Credentials-Importation/AnaplanCertPasscode.py
@@ -0,0 +1,8 @@
+def get_anaplan_Certcredentials():
+    privateKey='G:/Shared drives/Confidential.Finance/Finance.Squad/Confidential Finance/FP&A/PowerBi/1. Datasets and Uploads/Certificates/finance_telnyx_com/finance_telnyx_com/finance_telnyx.pem'
+    publicKey='G:/Shared drives/Confidential.Finance/Finance.Squad/Confidential Finance/FP&A/PowerBi/1. Datasets and Uploads/Certificates/finance_telnyx_com/finance_telnyx_com/finance_telnyx_com.pem'
+    workspace_id='8a868cda7a25fa3c017a5eef499571c6'
+    Operating_model_id = '12F8564EAAD248E695354AD082AF9080'
+    Revenue_model_id = '88709F904F594AE5B117EB613CDFAEFA'
+    Commission_model_id = '59DFC679B631403EA820DB76DDDE75DB'
+    return privateKey, publicKey, workspace_id, Operating_model_id,Revenue_model_id, Commission_model_id
---
+++ Credentials-Importation/AnaplanPasscode.py
@@ -0,0 +1,8 @@
+def get_anaplan_credentials():
+    anaplan_email = 'segun@telnyx.com'
+    anaplan_password = '01020304Abcd'
+    workspace_id='8a868cda7a25fa3c017a5eef499571c6'
+    Operating_model_id = '12F8564EAAD248E695354AD082AF9080'
+    Revenue_model_id = '88709F904F594AE5B117EB613CDFAEFA'
+    Commission_model_id = '59DFC679B631403EA820DB76DDDE75DB'
+    return anaplan_email, anaplan_password, workspace_id, Operating_model_id,Revenue_model_id, Commission_model_id
